7659c0fcf63c7b3dc5ed448e5f9bc1c153d4803e
BelstrelGit <Belstrel@gmail.com>
2017-11-17 12:37:53 +0300

    HW02 Working

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -7,14 +7,12 @@ public class AuthorizedUser {
     private static int id = 1;
 
     public static int id() {
-
-               return id;
+        return id;
     }
 
     public static void setId(int id) {
         AuthorizedUser.id = id;
     }
-
 
     public static int getCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
