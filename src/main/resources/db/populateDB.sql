54977d139b4d117bcc60b9ffbd193bdec9b1ab4b
BelstrelGit <Belstrel@gmail.com>
2017-12-12 23:51:02 +0300

    HW4_home_work_test_fix

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
 DELETE FROM meals;
 
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -12,14 +13,16 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
 
-INSERT INTO meals (user_id, datetime, description, calories) VALUES
-  (100000, '2015-05-31 08:02:01.00000', 'плов', 550 ),
-  (100000, '2015-05-31 12:00:00.000000', 'плов', 550 ),
-  (100000, '2015-05-31 14:00:00.000000', 'мясо', 1550),
-  (100001,' 2015-05-31 09:00:00.000000', 'плов', 550),
-  ( 100001,'2015-05-31 12:00:00.000000', 'мясо', 1550),
-  ( 100001,'2015-05-31 15:00:00.000000', 'плов', 550);
 
+INSERT INTO meals (date_time, description, calories, user_id) VALUES
+  ('2015-05-30 10:00:00', 'Завтрак', 500, 100000),
+  ('2015-05-30 13:00:00', 'Обед', 1000, 100000),
+  ('2015-05-30 20:00:00', 'Ужин', 500, 100000),
+  ('2015-05-31 10:00:00', 'Завтрак', 500, 100000),
+  ('2015-05-31 13:00:00', 'Обед', 1000, 100000),
+  ('2015-05-31 20:00:00', 'Ужин', 510, 100000),
+  ('2015-06-01 14:00:00', 'Админ ланч', 510, 100001),
+  ('2015-06-01 21:00:00', 'Админ ужин', 1500, 100001);
 
 -- DELETE FROM meals  WHERE meal_id=1 AND user_id =100000;
 -- INSERT INTO meals (meal_id,user_id, datetime, description, calories) VALUES
